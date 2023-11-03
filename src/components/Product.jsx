34ff673d7a7d6708511d7ef4bb51a4de03fa79d1
Ayushkhamkar <113784735+Ayushkhamkar@users.noreply.github.com>
2023-11-04 00:42:34 +0530

    product

---
+++ src/components/Product.jsx
@@ -0,0 +1,17 @@
+import "./Product.css";
+
+
+function Product(){
+  return (
+    <div>
+      <h2>Tilte of Product</h2>
+      <strong> RS200</strong>
+      <p>5star</p>
+      <img/>
+      <button type="submit" className="btn btn-warning">Add Product</button>
+    </div>
+  )
+}
+
+
+export default Product;
